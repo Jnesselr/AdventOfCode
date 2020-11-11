5cb7ccd58db18668f2c2a87879764c2922547642
Justin Nesselrotte <admin@jnesselr.org>
2020-11-11 00:42:18 -0700

    Initial commit

---
+++ aoc/y2019/d1.py
@@ -0,0 +1,15 @@
+class Y2019D1(object):
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def part1(self):
+        pass
+
+    def part2(self):
+        pass
+
+
+if __name__ == '__main__':
+    code = Y2019D1("2019/1.txt")
+    code.part1()
+    code.part2()
